3d895606130c1083e2134d34d90cb2567ab6e901
AsimZulfiqar67 <HighVoltages@github.com>
2018-11-02 19:11:57 +0500

    SUMMARY

--- HumanAssistantRobot-master/app/src/main/java/com/example/asimz/humanassistantrobot/Survillence.java
+++ HumanAssistantRobot-master/app/src/main/java/com/example/asimz/humanassistantrobot/Survillence.java
@@ -5,15 +5,26 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.VideoView;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
 
 public class Survillence extends AppCompatActivity {
-
+    public MqttAndroidClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,6 +32,40 @@ public class Survillence extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survillence);
+
+        String clientId = MqttClient.generateClientId();
+        client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://m15.cloudmqtt.com:14614",
+                        clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        options.setUserName("bqzbdodo");
+        options.setPassword("5oU2W_QN2WD8".toCharArray());
+
+
+
+        try {
+            IMqttToken token = client.connect(options);
+
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d("HomeAutomation", "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d("HomeAutomation", "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+
 
         WebView myWebView = (WebView) findViewById(R.id.videoView2);
         myWebView.loadUrl("http://192.168.43.47:8081");
@@ -33,5 +78,177 @@ public class Survillence extends AppCompatActivity {
         });
     }
 
+    public void moveForward(View view) {
+        String topic = "survillence";
+        String payload = "fwd";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
 
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+    public void moveBackward(View view) {
+        String topic = "survillence";
+        String payload = "bwd";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+    public void moveRight(View view) {
+        String topic = "survillence";
+        String payload = "right";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+    public void moveLeft(View view) {
+        String topic = "survillence";
+        String payload = "left";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+    public void camUp(View view) {
+        String topic = "survillence";
+        String payload = "camUp";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+    public void camDown(View view) {
+        String topic = "survillence";
+        String payload = "camDown";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+    public void RightHandUp(View view) {
+        String topic = "survillence";
+        String payload = "RightHandUp";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+
+    public void RightHandDown(View view) {
+        String topic = "survillence";
+        String payload = "RightHandDown";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+
+    public void LeftHandDown(View view) {
+        String topic = "survillence";
+        String payload = "LeftHandDown";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+
+    public void LeftHandUp(View view) {
+        String topic = "survillence";
+        String payload = "LeftHandUp";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+
+    public void HeadRight(View view) {
+        String topic = "survillence";
+        String payload = "HeadRight";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
+
+    public void HeadLeft(View view) {
+        String topic = "survillence";
+        String payload = "HeadLeft";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+        // Do something in response to button
+    }
 }
